a9c238fc473355ad9bfb4f398688fa1d468ff643
Yuvraj-dizayn <108083735+yuvraj0502@users.noreply.github.com>
2024-07-31 14:06:55 +0530

    HackerRank C++ Class Templates

---
+++ HackerRank/Easy/C++ Class Templates.cpp
@@ -0,0 +1,55 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <class cls>
+class AddElements{
+  cls ele1;
+  public:
+  AddElements(cls arg)
+  {
+    ele1=arg;
+  }
+cls add(cls ele2)
+{
+  return (ele1+ele2);
+}
+cls concatenate(cls ele2)
+{
+  return (ele1+ele2);
+}
+};
+
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
