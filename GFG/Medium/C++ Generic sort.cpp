e951cd2fd0cb108eef494487e00f6d190b73895e
Yuvraj-dizayn <108083735+yuvraj0502@users.noreply.github.com>
2024-08-02 15:09:05 +0530

    C++ Generic sort

---
+++ GFG/Medium/C++ Generic sort.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+template <class T>
+void sortArray(T a[], int n)
+{ 
+	//Add your code here.
+	for(int i=0;i<n;i++){
+	    for(int j=i+1;j<n;j++){
+	        if(a[j]<a[i]){
+	            T temp=a[j];
+	            a[j]=a[i];
+	            a[i]=temp;
+	        }
+	    }
+	}
+}
+
+template <class T>
+void printArray(T a[], int n)
+{
+	//Add your code here.
+	for(int i=0;i<n;i++){
+	    cout <<  a[i] << " " ;
+	}
+	cout << endl;
+}
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	int n, q, i;
+	cin>>n>>q;
+	
+	int intArr[n];
+	string strArr[n];
+    float floatArr[n];
+    
+	switch(q)
+	{
+	    case 1:
+	    for(i=0; i<n; i++)
+	    {
+	        cin>>intArr[i];
+	    }
+	    sortArray(intArr, n);
+    	printArray(intArr, n);
+    	break;
+        
+        case 2:
+	    for(i=0; i<n; i++)
+	    {
+	        cin>>strArr[i];
+	    }
+	    sortArray(strArr, n);
+    	printArray(strArr, n);
+    	break;
+    	
+        case 3:
+	    for(i=0; i<n; i++)
+	    {
+	        cin>>floatArr[i];
+	    }
+	    sortArray(floatArr, n);
+    	printArray(floatArr, n);
+    	break;
+	}
+    }
+
+	return 0;
+}
+
+// } Driver Code Ends
