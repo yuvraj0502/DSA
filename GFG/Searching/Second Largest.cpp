0d275104fcedc2dfa9880c34bb14b32ccc1a8911
Yuvraj-dizayn <108083735+yuvraj0502@users.noreply.github.com>
2024-08-06 22:34:07 +0530

    second largest

---
+++ GFG/Searching/Second Largest.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int print2largest(vector<int> &arr) {
+        // Code Here
+        sort(arr.rbegin(),arr.rend());
+        int max=arr[0];
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=max){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.print2largest(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
