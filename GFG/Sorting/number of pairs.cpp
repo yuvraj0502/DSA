d7d1ca608819ee1127cfad0fb548cffe057dd500
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-08-29 08:28:24 +0530

    number of pairs

---
+++ GFG/Sorting/number of pairs.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long countPairs(vector<int> &arr, vector<int> &brr) {
+        // Your Code goes here.
+        int count=0;
+        sort(brr.begin(),brr.end());
+        for(int i:arr){
+            for(int j:brr){
+                if(j*log(i)>i*log(j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        vector<int> ex;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            ex.push_back(num);
+        a = ex;
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        int num2;
+        while (ss >> num2)
+            b.push_back(num2);
+
+        Solution ob;
+        cout << ob.countPairs(a, b) << endl;
+    }
+}
+// } Driver Code Ends
