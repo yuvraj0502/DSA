bfde92b0169c25f4918b9305a48e7c912673ce21
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-08-29 12:37:26 +0530

    Sorting Elements of an Array by Frequenc

---
+++ GFG/Sorting/Sorting Elements of an Array by Frequenc.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Complete this function
+    // Function to sort the array according to frequency of elements.
+    vector<int> sortByFreq(vector<int>& arr) {
+        // Your code here
+        unordered_map<int,int>freq;
+        for(int i:arr){
+            freq[i]++;
+        }
+        sort(arr.begin(),arr.end(),[&freq](int first,int second){
+            if(freq[first]==freq[second]){
+                return first<second;
+            }
+            return freq[first]>freq[second];
+        });
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution obj;
+        vector<int> v;
+        v = obj.sortByFreq(arr);
+        for (int i : v)
+            cout << i << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
