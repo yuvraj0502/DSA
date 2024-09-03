f9be7af7617de6b4f192dd5aaf2e9f12cb13c1c8
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-03 09:21:55 +0530

    number of  pairs

--- GFG/Sorting/number of pairs.cpp
+++ GFG/Sorting/number of pairs.cpp
@@ -8,14 +8,22 @@ class Solution {
   public:
     long long countPairs(vector<int> &arr, vector<int> &brr) {
         // Your Code goes here.
-        int count=0;
-        sort(brr.begin(),brr.end());
-        for(int i:arr){
-            for(int j:brr){
-                if(j*log(i)>i*log(j)){
-                    count++;
-                }
-            }
+        vector<double>vec1,vec2;
+        for(int i=0;i<arr.size();i++){
+            double x=log10(arr[i]);
+            x/=arr[i];
+            vec1.push_back(x);
+        }
+        for(int i=0;i<brr.size();i++){
+            double x=log10(brr[i]);
+            x/=brr[i];
+            vec2.push_back(x);
+        }
+        sort(vec2.begin(),vec2.end());
+        long long count=0;
+        for(int i=0;i<vec1.size();i++){
+            int index=lower_bound(vec2.begin(),vec2.end(),vec1[i])-vec2.begin();
+            count+=index;
         }
         return count;
     }
