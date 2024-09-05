5504149ace0a89a777ea12ed1c0ae53c4f4555fa
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-05 10:58:57 +0530

    Count zeros in a sorted matrix

---
+++ 1_Learning_C++/2D Array/Count zeros in a sorted matrix.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+public:
+	int countZeros(vector<vector<int>>A)
+	{
+		//code here
+		int row=0;
+		int column=A.size()-1;
+		int count=0;
+		
+		while(row<A.size() && column>=0){
+		    if(A[row][column]==0){
+		        count=count+column+1;
+		        row++;
+		    }
+		    else{
+		        column--;
+		    }
+		}
+		return count;
+	}
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>>A(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n;j++){
+                cin>>A[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.countZeros(A)<<'\n';
+    }
+    return 0;
+}
+
+// } Driver Code Ends
