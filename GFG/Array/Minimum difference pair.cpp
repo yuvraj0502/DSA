49161ad521ede4740a1e2e2c8ad1350d9fce3af5
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-05 11:42:29 +0530

    Minimum difference pair

---
+++ GFG/Array/Minimum difference pair.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+   	int  minimum_difference(vector<int>nums){
+   	    // Code here
+   	    int min_diff=INT_MAX;
+   	    sort(nums.begin(),nums.end());
+   	    for(int i=0;i<nums.size()-1;i++){
+   	        min_diff=min((nums[i+1]-nums[i]),min_diff);
+   	    }
+   	    return min_diff;
+   	}    
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < nums.size(); i++)cin >> nums[i];
+		Solution ob;
+		int ans = ob.minimum_difference(nums);
+		cout << ans <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
