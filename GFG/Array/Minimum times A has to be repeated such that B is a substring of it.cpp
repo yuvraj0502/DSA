68bc3b0a0e87362ffab3012b70d3f65ed4138010
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-06 10:13:59 +0530

    Minimum times A has to be repeated such that B is a substring of it

---
+++ GFG/Array/Minimum times A has to be repeated such that B is a substring of it.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int minRepeats(string A, string B) {
+        // code here
+        int repect=1;
+        string substring=A;
+        for(int i=0;i<800;i++){
+            if(substring.find(B)!=string::npos){
+                return repect;
+            }
+            substring+=A;
+            repect++;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ",&t);
+    while (t--) {
+        string A,B;
+        getline(cin,A);
+        getline(cin,B);
+
+        Solution ob;
+        cout << ob.minRepeats(A,B) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
