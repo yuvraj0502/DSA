8f6795c7a43ff6ffe7b1b8601145506bb65497ad
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-07 13:00:33 +0530

    Binary matrix having maximum number of 1s

---
+++ 1_Learning_C++/2D Array/Binary matrix having maximum number of 1s.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int N) {
+        //code here
+        int i=0;
+        int j=N-1;
+        int row=0;
+        int count=0;
+        while(j>=0 && i<N){
+            if(mat[i][j]==1){
+                count++;
+                row=i;
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return {row,count};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n, vector<int> (n));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cin >> arr[i][j];
+        Solution obj;
+        vector<int> ans = obj.findMaxRow(arr, n);
+        for(int val : ans) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
