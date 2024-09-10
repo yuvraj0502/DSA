3bfc9d584e2b67d712e5237d4699d31552e841fd
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-10 11:15:51 +0530

    Sort the Half Sorted

---
+++ GFG/Array/Sort the Half Sorted.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+        void sortHalves (int arr[], int n)
+        {
+            // your code here
+            sort(arr,arr+n);
+            
+        }
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+		int arr[n];
+
+		for (int i = 0; i < n; ++i)
+			cin >> arr[i];
+        
+        Solution ob;
+        
+		ob.sortHalves (arr, n);
+		for (int i = 0; i < n; ++i)
+			cout << arr[i] << " ";
+		cout << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
