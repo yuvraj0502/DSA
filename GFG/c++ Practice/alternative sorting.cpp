589998b0f0672de85298594985c916cec26c95dc
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-13 10:08:30 +0530

    alternative sorting

---
+++ GFG/c++ Practice/alternative sorting.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        // Your code goes here
+        sort(arr.begin(),arr.end());
+        vector<int>sorted;
+        int start=0;
+        int end=arr.size()-1;
+        while(start<end){
+            sorted.push_back(arr[end]);
+            sorted.push_back(arr[start]);
+            start++;
+            end--;
+        }
+        if(arr.size()%2!=0){
+            sorted.push_back(arr[arr.size()/2]);
+        }
+        return sorted;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        // cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
