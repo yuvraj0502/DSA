4b2a62bfed17b37d1f043ab57b7e78419f171274
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-25 16:15:42 +0530

    remove cycle linked list

---
+++ LinkedList/remove cycle linked list.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+void makeCycle(node* &head,int pos){
+    node* temp=head;
+    node* startNode;
+    int count=1;
+
+    while(temp->next!=NULL){
+        if(count==pos){
+            startNode=temp;
+        }
+        temp=temp->next;
+        count++;
+    }
+    temp->next=startNode;
+}
+
+bool detectCyle(node* &head){
+    node* slow=head;
+    node* fast=head;
+
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast){
+            cout<<'Cycle is deteced!!'<<endl;
+            return true;
+        }
+    }
+    coun<<"Cycle is not deteced!!"<<endl;
+    return false; 
+}
+
+void removeCycle(node* &head){
+    node* slow=head;
+    node* fast=head;
+
+    do
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    } while (slow!=fast);
+
+    fast=head;
+    while(slow->next!=fastt->next){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    slow->next=NULL;
+    
+}
+
+
+void insertAtTail(node*  &head,int val){
+    node* n=new node(val);
+
+    if(head==NULL){
+        head=n;
+        return ;
+    }
+
+    node* temp=head;
+
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+    node* head=NULL;    
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    insertAtTail(head,5);
+    insertAtTail(head,6);
+    makeCycle(head,4);
+    cout<<detectCyle(head)<<endl; 
+    display(head);
+}
