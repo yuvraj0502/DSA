24f4bb5bd66f019b0bcfc2b4b6005cc6b9df62ce
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-09-26 13:02:03 +0530

    Kth from End of Linked List

---
+++ GFG/LinkedList/Kth from End of Linked List.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+// Function to find the data of kth node from the end of a linked list.
+class Solution {
+  public:
+    int getKthFromLast(Node *head, int k) {
+        // Your code here
+        Node* temp=head;
+        for(int i=0;i<k;i++){
+            if(temp==NULL){
+                return -1;
+            }
+            temp=temp->next;
+        }
+        while(temp!=NULL){
+            temp=temp->next;
+            head=head->next;
+        }
+        return head->data;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        cout << ob.getKthFromLast(head, x) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
