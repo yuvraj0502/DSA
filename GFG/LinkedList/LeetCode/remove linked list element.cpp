dbb15160edae78f267c5d4de99bb5e33849044aa
YuvrajGupta <yuvrajguptaa0502@gmail.com>
2024-10-09 12:51:04 +0530

    remove linked list element

---
+++ GFG/LinkedList/LeetCode/remove linked list element.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* temp=new ListNode(0);
+        temp->next=head;
+        ListNode* prev=temp;
+        ListNode* curr=head;
+
+        while(curr!=NULL){
+             if(curr->val==val){
+                prev->next=curr->next;
+             }else{
+                prev=curr;
+             }
+             curr=curr->next;
+        }
+        ListNode* newHead=temp->next;
+        delete temp;
+        return newHead;
+    }
+};
