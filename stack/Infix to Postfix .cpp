fe91ed6d2076065a96c6b644594b366f2e8cc6c2
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-10-24 12:20:13 +0530

    Infix to Postfix

---
+++ stack/Infix to Postfix .cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<stack>
+#include<math.h>
+using namespace std;
+
+ 
+int president(char c){
+    if(c=='^'){
+        return 3;
+    }
+    else if(c=='*' || c=='/'){
+        return 2;
+    }
+    else if(c=='+' || c=='-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+string infixToPostfix(string s){
+    stack<char>st;
+    string result;
+
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z'){
+            result+=s[i];
+        }
+        else if(s[i]=='('){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                result+=st.top();
+                st.pop(); 
+            }
+            if(!st.empty()){
+                st.pop();
+            }
+        }
+        else{
+            while(!st.empty() && president(st.top())>president(s[i])){
+                result+=st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+
+    while(!st.empty()){
+        result+=st.top();
+        st.pop();
+    }
+    return result;
+}
+
+
+int main(){
+     cout<<infixToPostfix("(a-b/c)*(a/k-l)")<<endl;
+     return 0;
+}
