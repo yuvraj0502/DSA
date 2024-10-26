393c6fef86fa13b1b073c317e2818021ffa7c6ad
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-10-26 16:15:58 +0530

    implement stack using queue

---
+++ LeetCode/stack/implement stack using queue.cpp
@@ -0,0 +1,39 @@
+class MyStack {
+private:
+    queue<int>qu;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        qu.push(x);
+        for(int i=0;i<qu.size()-1;i++){
+            qu.push(qu.front());
+            qu.pop();
+        }
+    }
+    
+    int pop() {
+        int val=qu.front();
+        qu.pop();
+        return val;
+    }
+    
+    int top() {
+        return qu.front();
+    }
+    
+    bool empty() {
+        return qu.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
