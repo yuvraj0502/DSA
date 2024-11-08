08ce2b12af34c0dcdbcc599c826bde44cd4eb39c
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-11-08 17:57:34 +0530

    Queue Type

---
+++ Queue/Queue Type.cpp
@@ -0,0 +1,29 @@
+1. simple queue: Simple Queue simply follows FIFO Structure. We can only insert 
+                 the element at the back and remove the element from the front of 
+                 the queue.
+
+2. Double end queue: In a double-ended queue the insertion and deletion operations,
+                     both can be performed from both ends. 
+
+                     1.Input Restricted Queue: This is a simple queue. In this type 
+                     of queue, the input can be taken from only one end but deletion 
+                     can be done from any of the ends.
+
+                     2.Output Restricted Queue:This is also a simple queue. In this type 
+                     of queue, the input can be taken from both ends but deletion can be 
+                     done from only one end.
+
+3. Circular queue:  This is a special type of queue where the last position is connected 
+                    back to the first position. Here also the operations are performed 
+                    in FIFO order.
+
+4.  Priority queue: A priority queue is a special queue where the elements are accessed 
+                    based on the priority assigned to them. 
+
+                    1.Ascending Priority Queue:In Ascending Priority Queue, the elements 
+                    are arranged in increasing order of their priority values. Element 
+                    with smallest priority value is popped first.
+
+                    2.Descending Priority Queue: In Descending Priority Queue, the 
+                    elements are arranged in decreasing order of their priority values. 
+                    Element with largest priority is popped first.
