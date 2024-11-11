9adf40afd102fa1096797c676140ba34f48c70fc
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-11-11 09:31:19 +0530

    introducation

---
+++ Queue/Circular Queue/introducation.cpp
@@ -0,0 +1,12 @@
+Circular queue is an extended version of the simple queue, where the last element is
+connect the first element of the position.This queue work from FIFO and it is called
+"Ring Buffer".
+
+Operation
+
+1. front:get the front item from the queue.
+2. rear: get the last item from the queue.
+3. enqueue(value): This function is used to insert the element into the circular queue. 
+   In cicular queue ,the new element is always inserted at the rear  position.
+4. dequeue(): This function is used to deleted the element into circular queue,In cicular
+   queue,the element is always deleted at the front position.
