a397471b3680b3b386fd1c5b89e11f0e41bce7b3
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-11-14 12:39:10 +0530

    Method of priority queue

---
+++ Queue/Priority queue/Method of priority queue.cpp
@@ -0,0 +1,10 @@
+1. empty() : return whether the queue is empty.
+2. size(): return the size of queue.
+3. top() : return the reference to the topmost element of the queue.
+4. push() : adds the element at the end of the queue.
+5. pop() : delete the first element of queue.
+6. swap(): Used to swap the contents of two queues provided the queues must be of the   
+           same type, although sizes may differ.
+7. emplace() : insert the new element in the queue container.
+8. value_type() : Represents the type of object stored as an element in a priority_queue. 
+                  It acts as a synonym for the template parameter.
