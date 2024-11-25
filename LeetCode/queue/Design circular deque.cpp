5c982d2589fd78ef7f7f9d39d98837f6ddfd73d1
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-11-25 12:28:58 +0530

    Design circular deque

---
+++ LeetCode/queue/Design circular deque.cpp
@@ -0,0 +1,78 @@
+class MyCircularDeque {
+    private:
+    vector<int> deque;
+    int capacity;  
+    int front;     
+    int rear;      
+    int size;    
+public:
+    MyCircularDeque(int k) {
+        capacity = k;
+        deque.resize(k);
+        front = 0;
+        rear = 0;
+        size = 0;
+    }
+    
+    bool insertFront(int value) {
+        if (isFull()) {
+            return false;
+        }
+        front = (front - 1 + capacity) % capacity;  
+        deque[front] = value;
+        size++;
+        return true;
+    }
+    
+    bool insertLast(int value) {
+        if (isFull()) {
+            return false;
+        }
+        deque[rear] = value;
+        rear = (rear + 1) % capacity;  
+        size++;
+        return true;
+    }
+    
+    bool deleteFront() {
+        if (isEmpty()) {
+            return false;
+        }
+        front = (front + 1) % capacity;  
+        size--;
+        return true;
+    }
+    
+    bool deleteLast() {
+        if (isEmpty()) {
+            return false;
+        }
+        rear = (rear - 1 + capacity) % capacity;  
+        size--;
+        return true;
+    }
+    
+    int getFront() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return deque[front];
+    }
+    
+    int getRear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return deque[(rear - 1 + capacity) % capacity];
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
+
+  
+};
