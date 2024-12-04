e4c44ddfb80c44679937d3114023771500473918
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-12-04 13:44:46 +0530

    single number

---
+++ DSA Series By Apna College/Array/single number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        for(auto i:freq){
+            if(i.second==1){
+                return i.first;
+            }
+        }
+        return -1; 
+    }
+};
