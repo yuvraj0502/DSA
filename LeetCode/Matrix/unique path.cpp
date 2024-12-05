0f793f24ac9a3ef8a9f4fb61d8b1b731df699945
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-12-05 09:34:27 +0530

    unique path

---
+++ LeetCode/Matrix/unique path.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>result(m,vector<int>(n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                result[i][j]=result[i-1][j]+result[i][j-1];
+            }
+        }
+        return result[m-1][n-1];
+    }
+};
