f7acd26262e1f270947e65af2fd9a5b2fc141564
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-12-05 17:41:00 +0530

    pow(x,n)

--- LeetCode/Matrix/unique path2.cpp
+++ LeetCode/Matrix/unique path2.cpp
@@ -22,13 +22,10 @@ public:
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 if(obstacleGrid[i][j]==0){
-                    result[i][j]=result[i-1][j]+result[i][i-1];
+                    result[i][j]=result[i-1][j]+result[i][j-1];
                 }
             }
         }
         return result[m-1][n-1];
-
-        
-
     }
 };
