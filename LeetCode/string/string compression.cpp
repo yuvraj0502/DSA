0921cfc0a9fe7110cf2828a3bd6984d75c319631
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-12-18 20:15:20 +0530

    string compression

---
+++ LeetCode/string/string compression.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int idx=0;
+        for(int i=0;i<chars.size();i++){
+            char ch = chars[i];
+            int count = 0;
+
+            while(i<chars.size() && chars[i] == ch){
+                count++;
+                i++;
+            }
+
+            if(count==1){
+                chars[idx]=ch;
+                idx++;
+            }else{
+                chars[idx]=ch;
+                idx++;
+                string str=to_string(count);
+                for(char i: str){
+                    chars[idx]=i;
+                    idx++;
+                }
+            }
+            i--;
+        }
+        return idx;
+    }
+};
