44bd4a8cec30e4d23cabdf9ac9c8fa98ea0fbe77
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-12-19 08:48:11 +0530

    string introducation

---
+++ string/introducation.cpp
@@ -0,0 +1,39 @@
+strings are sequences of characters stored in a char array. 
+strings are used to store words and text. 
+They are also used to store data, such as numbers and other types of information. 
+
+3 way take the string in the input
+  1.cin
+  2.getline
+  3.stringstream
+
+
+function of string:
+
+length() : This function return the length of string
+swap() : This function is used to swap to the value of 2 string
+size(): This function used the find the size of string
+resize(): This funtion used to resize the length of the string up to the given number of characters
+push_back(): This function is used to push the passed characters at the end of string
+pop_back(): This function is used the pop the last element at the string
+clear(): This function is used the remove all the element the string
+strncmp(): This function compares at most the first num bytes of both passed strings.
+strncpy(): This function used the copy string
+strrchr(): This function locates the last occurrence of a character in the string
+strcat(): This function appends a copy of the source string to the end of the destination string
+find():This function is used to search for a certain substring inside a string and returns the position of the first character of the substring. 
+replcae(): This function is used to replace each element in the range[first,last]that is equal to old value with new value
+substr():This function is used to create a substring from a given string
+compare(): This function is used to compate 2 string and return the int value
+erase(): This function is used to remove a certain part of a string.
+begin():This function is return an iterator pointing to the beginning of the string
+end():This function is return an iterotor that point to the end of the string
+rfind():This function used to the find the last occurrence of the string
+rbegin():This function return a reverse iterator pointing to the end of the string. 
+rend():This function return a reverse iterator pointing to the beginning of the string.
+cbegin():This function return a const_iterator pointing to the beginning of the string.
+cend():This function return a const_iterator pointing to the end of the string
+crbegin():This function return a const_reverse_iterator pointing to the end of the string.
+crend():This function return a const_reverse_iterator pointing to the beginning of the string
+
+
