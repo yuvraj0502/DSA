8eed6753e8810be46378f7ec6c7351e22b4dc125
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2024-12-30 12:34:57 +0530

    jump game 2

---
+++ LeetCode/Array/jump game2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0){
+            return 1;
+        }
+        
+        int jumps = 0;
+        int farthest = 0;
+        int currentEnd = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;  
+                currentEnd = farthest;  
+                if (currentEnd >= n - 1) break;
+            }
+        }
+        return jumps;
+    }
+};
