b0433d2d29cd251879bf21247514bd96ac70a8f5
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-01-03 19:23:52 +0530

    Greedy Algorithms introducation

---
+++ Greedy Algorithms/introdaction.cpp
@@ -0,0 +1,21 @@
+A Greedy Algorithm decides what to do in each step,only based on the current situation,
+without the thought of how the total problem looks like.
+In other words, a greedy algorithm make the locally optimal choice the each step, 
+hoping to find the global optimam solution in the end.
+
+Greedy is an algorithm paradigm that builds up a solution piece by piece, always the 
+choosing the next piece that offers the most obvious and immediate benefit. Greedy 
+algorithms are used for optimization problems. 
+At every step, we can make a choice that looks best at the moment, and we get the optimal 
+solution to the complete problem.
+
+
+Two Properties must be the true for the problem for the greedy algorithm to work
+
+1.Greedy choice property:Means that the problem is so that the solution (the global 
+optimum) can be reached by making greedy choices in each step (locally optimal choices).
+
+2.optimal substructure:Means that the optimal solution of the problem, is a collection
+of the optimal solutions to sub-problem. so solving smaller parts the problem locally
+(by making greedy choice) contributes to the overall solution.
+
