6824e98ec90375db740ce1ea7250d8313efb44e7
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-01-09 12:32:53 +0530

    Remove Duplicate letter

---
+++ LeetCode/Greedy/Remove Duplicate letter.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std:
+
+class Solution {
+public:
+
+    string removeDuplicateLetters(string s) {
+        vector<int>count(26,0);
+        vector<bool>visited(26,false);
+        for(char c:s){
+            count[c-'a']++;
+        }
+        string result="";
+        for(char c:s){
+            count[c-'a']--;
+            if(visited[c-'a']){
+                continue;
+            }
+            while(!result.empty()&&result.back()>c&&count[result.back()-'a']>0){
+                visited[result.back()-'a']=false;
+                result.pop_back();
+            }
+            result.push_back(c);
+            visited[c-'a']=true;
+        }
+        return result;
+    }
+};
