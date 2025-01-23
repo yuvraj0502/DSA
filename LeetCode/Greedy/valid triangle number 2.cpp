fac4406ebc8421035aa7daa68070846cc94813dd
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-01-23 22:38:22 +0530

    valid triangle number 2

---
+++ LeetCode/Greedy/valid triangle number 2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int count=0;
+        sort(nums.begin(),nums.end());
+        for(int k=2;k<nums.size();k++){
+            int i=0;
+            int j=k-1;
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    count+=(j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+};
