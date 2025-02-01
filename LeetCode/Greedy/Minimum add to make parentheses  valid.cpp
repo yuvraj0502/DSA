51372dee9c5511657279c714b0f58dabf9cfd57d
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-02-01 15:15:59 +0530

    Minimum add to make paretheses valid

---
+++ LeetCode/Greedy/Minimum add to make parentheses  valid.cpp
@@ -0,0 +1,19 @@
+class Solutio {
+     public:   
+         int minAddToMakeValid(string s){
+            int open=0;
+            int close=0;
+            for(char c:s){
+                if(c=='('){
+                    open++;
+                }
+                else if(open>0){
+                    open--;
+                }
+                else{
+                    close++;
+                }
+            }
+            return open+close;
+         }
+};
