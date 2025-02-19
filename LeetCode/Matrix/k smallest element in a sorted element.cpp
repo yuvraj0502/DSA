368beb9eaf14e3ae50e56a7ad3d9eb81a99aec62
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-02-19 15:40:58 +0530

    k smallest element in a sorted element(using priority queue)

--- LeetCode/Matrix/k smallest element in a sorted element.cpp
+++ LeetCode/Matrix/k smallest element in a sorted element.cpp
@@ -1,15 +1,13 @@
 class Solution{
     public:
       int kthSmallest(vector<vector<int>>&matrix,int k){
-         priority_queue<int>pq;
+         vector<int>result;
          for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                pq.push(matrix[i][j]);
-                if(pq.size()>k){
-                    pq.pop();
-                }
+                result.push_back(matrix[i][j]);
             }
          }
-         return pq.top();
+         sort(result.begin(),result.end());
+         return result[k-1];
       }
 };
