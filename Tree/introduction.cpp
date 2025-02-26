8e8ee238e33c5ef0ab71a9ac4ede177917ac2e23
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-02-26 09:47:05 +0530

    introduction

---
+++ Tree/introduction.cpp
@@ -0,0 +1,68 @@
+-> Tree data structure is hierarchial structure that is used to represent and organize
+   data in the form of parent child relationship.
+-> The Topmost element is root element and the elements below it are called children.
+-> The elements that are at the same level are called siblings.
+-> The elements that are at the bottom  are called leaves.
+
+Type of the tree:
+1. Binary Tree 
+2. Ternary Tree
+3. N-ary Tree or Generic Tree
+
+Binary Tree:
+-> A tree in which each node has at most 2 children.
+-> The children are called left child and right child.
+-> The left child is always inserted,removed,visited,processed first and then 
+   the right child.
+
+
+Types of Binary Tree:
+1. Full Binary Tree:
+-> A binary tree in which each node has exactly 0 or 2 children.
+
+
+2. Complete Binary Tree:
+-> A binary tree in which all the levels are completely filled except possibly 
+   the last level.
+-> In the last level, all the nodes are as left as possible.
+-> A binary tree in which all the levels are completely filled except possibly 
+   the last level and the last level is filled from left to right.
+
+3. Perfect Binary Tree:
+-> A binary tree in which all the levels are completely filled and 
+   all the nodes have 2 children.
+
+4. Balanced Binary Tree:
+-> A binary tree in which the height of the left subtree and the height of the 
+   right subtree of each node differ by at most 1 or 0.
+
+
+Ternary Tree:
+-> A tree in which each node has at most 3 children.
+-> The children are called left child, middle child and right child.
+-> The left child is always inserted,removed,visited,processed first and then the 
+   middle child and then the right child.
+
+
+N-ary Tree or Generic Tree:
+-> A tree in which each node has at most N children
+-> The children are called first child, second child, third child and so on.
+-> The first child is always inserted,removed,visited,processed first and then the 
+   second child and then the third child and so on.
+
+
+Tree Traversal:
+1. Depth First Traversal:
+-> In this traversal, the nodes are visited in depthwise order.
+-> The nodes are visited from the root to the leaf nodes.
+-> The nodes are visited in the following order:
+   a. Preorder Traversal
+   b. Inorder Traversal
+   c. Postorder Traversal
+
+2. Breadth First Traversal:
+-> In this traversal, the nodes are visited in breadthwise order.
+-> The nodes are visited level by level.
+-> The nodes are visited in the following order:
+   a. Level Order Traversal
+
