4a6de8bf74b37f14a4f1e36cf6a02da3291ee8da
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-03-15 15:46:34 +0530

    Check if two trees are Mirror

---
+++ Tree/Binary Tree/Check if two trees are Mirror.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int key){
+        data=key;
+        left=nullptr;
+        right=nullptr;
+    }
+}
+
+bool isMirror(Node* root1,Node* root2){
+    if(root1==nullptr && root2==nullptr){
+        return true;
+    }
+    if(root1==nullptr || root2==nullptr){
+        return false;
+    }
+    return (root1->data==root2->data) &&
+            isMirror(root1->left,root2->right) &&
+            isMirror(root1->right,root2->left);
+}
+
+int main(){
+    Node* root1=new Node(1);
+    root1->left=new Node(2);
+    root1->right=new Node(3);
+    root1->left->left=new Node(4);
+    root1->left->right=new Node(5);
+    root1->right->left=new Node(6);
+    root1->right->right=new Node(7);
+
+    Node* root2=new Node(1);
+    root2->left=new Node(3);
+    root2->right=new Node(2);
+    root2->left->left=new Node(7);
+    root2->left->right=new Node(6);
+    root2->right->left=new Node(5);
+    root2->right->right=new Node(4);
+
+    if(isMirror(root1,root2)){
+        cout<<"Yes";
+    }else{
+        cout<<"No";
+    }
+
+    return 0;
+}
