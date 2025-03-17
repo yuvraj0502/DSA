f34249dd15c902269ef914b2d7f27139a602f0a2
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-03-17 16:54:52 +0530

    introducation

---
+++ Tree/Breadth First Search (BFS)/introducation.cpp
@@ -0,0 +1,8 @@
+-> Breadth First Search (BFS) is a technique used to traverse a tree or graph.
+-> This also called the level order traversal technique.
+-> In this traversal, the nodes are visited level by level.
+-> The main idea of level order traversal is to traverse all the nodes of a 
+   lower level before moving to any of the nodes of a higher level.
+-> BFS is used to find the shortest path in an unweighted graph.
+
+
