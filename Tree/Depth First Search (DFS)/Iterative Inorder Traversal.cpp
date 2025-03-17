b2a3be436d7c2bc3c1d22d131e05795ecda17f94
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-03-17 12:55:10 +0530

    Iterative Inorder Traversal

---
+++ Tree/Depth First Search (DFS)/Iterative Inorder Traversal.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int key){
+        data=key;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+
+void inOrderTraversal(Node* root){
+    stack<Node*>st;
+    Node* curr=root;
+    while(curr!=nullptr || !st.empty()){
+        while(curr!=nullptr){
+            st.push(curr);
+            curr=curr->left;
+        }
+        curr=st.top();
+        st.pop();
+        cout<<curr->data<<" ";
+        curr=curr->right;
+    };
+}
+
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "inOrder Traversal: ";
+    inOrderTraversal(root);
+    return 0;
+}
